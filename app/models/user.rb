dfa8e491c7d1fbedbca3230090072d7a047c3434
Ilia Liamshin <lyamsh@yandex.ru>
2022-01-13 03:22:19 +0300

    test&validation: add case_usensitive for email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-  validates :email, uniqueness: true, presence: true
+  validates :email, uniqueness: { case_sensitive: false }, presence: true
   validates :password_digest, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  subject { User.new(email: "example@example.com", password_digest: "00000") }
+
   describe 'have attributes' do
-    it { is_expected.to respond_to(:email)}
-    it { is_expected.to respond_to(:password_digest)}
+    it { is_expected.to respond_to(:email) }
+    it { is_expected.to respond_to(:password_digest) }
+  end
+
+  describe 'validation' do
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_presence_of(:password_digest) }
+    it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
   end
 end
