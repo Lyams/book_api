c23fde3904bd1b4973658e4f17019d589daaef7b
Ilia Liamshin <lyamsh@yandex.ru>
2022-01-13 18:39:09 +0300

    feat: show User response json

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe "Api::V1::Users", type: :request do
     it "should show user" do
       get api_v1_user_url(user)
       expect(response).to have_http_status :success
+      expect((JSON::parse response.body)["email"]).to eq user.email
     end
   end
 end
