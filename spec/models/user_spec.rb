22f1a875e13230daa0d940e128580ddbf2aeb9b7
Ilia Liamshin <lyamsh@yandex.ru>
2022-01-13 03:39:49 +0300

    test@feat: add simple validation email and rspec model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,5 +12,6 @@ RSpec.describe User, type: :model do
     it { is_expected.to validate_presence_of(:email) }
     it { is_expected.to validate_presence_of(:password_digest) }
     it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
+    it { is_expected.to be_valid }
   end
 end
