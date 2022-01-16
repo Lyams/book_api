63f678f87f80ed37744a788187989f2897e222d9
Ilia Liamshin <lyamsh@yandex.ru>
2022-01-16 04:07:02 +0300

    feat: create user with test

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,22 @@
 class Api::V1::UsersController < ApplicationController
+
   def show
     render json: User.find(params[:id])
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user, status: :created
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+
 end
--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -1,12 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Users", type: :request do
-  let(:user) { create(:user) }
+  let(:user) { build(:user) }
   describe "GET #show" do
     it "should show user" do
+      user.save
       get api_v1_user_url(user)
       expect(response).to have_http_status :success
       expect((JSON::parse response.body)["email"]).to eq user.email
+    end
+  end
+
+  describe "POST #create" do
+    it "should create user" do
+      expect { post api_v1_users_url, params: {
+        user: { email: user.email, password: user.password } } }.to change { User.count }.by(1)
+      expect(response.status).to eq 201
+    end
+    context 'invalid data' do
+      before { user.save }
+      it "should not create user with taken email" do
+        expect { post api_v1_users_url, params: {
+          user: { email: user.email, password: "234" } } }.to change { User.count }.by(0)
+        expect(response.status).to eq(422)
+      end
     end
   end
 end
