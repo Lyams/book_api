22139a4491f2d42a7108a5e16b47d40e2adf1d7b
Ilia Liamshin <lyamsh@yandex.ru>
2022-01-19 02:59:18 +0300

    feat: Adds the show action for order

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -4,4 +4,14 @@ class Api::V1::OrdersController < ApplicationController
   def index
     render json: OrderSerializer.new(current_user.orders).serializable_hash
   end
+
+  def show
+    order = current_user.orders.find(params[:id])
+    if order
+      options = { include: [:products] }
+      render json: OrderSerializer.new(order, options).serializable_hash
+    else
+      head :not_found # 404
+    end
+  end
 end
--- spec/requests/api/v1/orders_spec.rb
+++ spec/requests/api/v1/orders_spec.rb
@@ -1,16 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Orders", type: :request do
-  describe "GET /index" do
-    let(:order) { create(:order)}
-    let(:product) {build(:product)}
+  let(:order){create :order}
+  let(:product){build :product}
+  before {product.user = order.user; product.save; order.update(products: [product])}
+  
+  describe "GET #show" do
+    it 'should show order' do
+      get api_v1_order_url(order),
+          headers: { Authorization: JsonWebToken.encode(user_id: order.user_id)}, as: :json
+      expect(response).to have_http_status(:success)
+      json_response = JSON.parse(response.body)
+      include_product_attr = json_response['included'][0]['attributes']
+      expect(include_product_attr['title']).to eq(order.products.first.title)
+    end
+    it 'should forbid show order' do
+      get api_v1_order_url(order), as: :json
+      expect(response).to have_http_status(:forbidden)
+    end
+  end
+
+  describe "GET #index" do
     it "should forbid orders for unlogged" do
       get api_v1_orders_url, as: :json
       expect(response).to have_http_status(:forbidden)
     end
     it "should show orders" do
-      product.user = order.user
-      order.update(products: [product])
       get api_v1_orders_url, headers: { Authorization: JsonWebToken.encode(user_id: order.user_id)}, as: :json
       expect(response).to have_http_status(:success)
       json_response = JSON.parse(response.body)
