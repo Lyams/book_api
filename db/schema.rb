86cdd23eca1fa679a509815aa486c7ed98911232
Ilia Liamshin <lyamsh@yandex.ru>
2022-01-20 13:41:00 +0300

    fix: Fix Email Unique Case UNsensitive

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_18_224222) do
+ActiveRecord::Schema.define(version: 2022_01_20_103433) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -44,7 +44,7 @@ ActiveRecord::Schema.define(version: 2022_01_18_224222) do
     t.string "password_digest", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index "lower(email)", name: "index_users_on_lowercase_email", unique: true
   end
 
   add_foreign_key "orders", "users"
