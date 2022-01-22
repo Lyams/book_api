ec8671772f6fcfee2ed8319e03a1836657e11721
Ilia Liamshin <lyamsh@yandex.ru>
2022-01-22 03:55:52 +0300

    refac: fix n+1 request and dry

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -10,11 +10,9 @@ class Api::V1::ProductsController < ApplicationController
   end
 
   def index
-    @products = Product.page(current_page).per(per_page).search(params)
-    options = { links: { first: api_v1_products_path(page: 1),
-                         last: api_v1_products_path(page: @products.total_pages),
-                         prev: api_v1_products_path(page: @products.prev_page),
-                         next: api_v1_products_path(page: @products.next_page),} }
+    @products = Product.includes(:user).page(current_page).per(per_page).search(params)
+    options = get_links_serializer_options 'api_v1_products_path', @products
+    options[:include] = [:user]
     render json: ProductSerializer.new(@products, options).serializable_hash
   end
 
