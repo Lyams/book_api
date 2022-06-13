143a05824a1a556fc6af10af5fe85c449e57f998
Ilia Liamshin <lyamsh@yandex.kz>
2022-06-13 20:02:43 +0300

    Updated gems

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,86 +10,83 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_220_124_233_856) do
+ActiveRecord::Schema[7.0].define(version: 2022_01_24_233856) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'pgcrypto'
-  enable_extension 'plpgsql'
+  enable_extension "pgcrypto"
+  enable_extension "plpgsql"
 
-  create_table 'good_job_processes', id: :uuid, default: -> { 'gen_random_uuid()' }, force: :cascade do |t|
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.jsonb 'state'
+  create_table "good_job_processes", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.jsonb "state"
   end
 
-  create_table 'good_jobs', id: :uuid, default: -> { 'gen_random_uuid()' }, force: :cascade do |t|
-    t.text 'queue_name'
-    t.integer 'priority'
-    t.jsonb 'serialized_params'
-    t.datetime 'scheduled_at'
-    t.datetime 'performed_at'
-    t.datetime 'finished_at'
-    t.text 'error'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.uuid 'active_job_id'
-    t.text 'concurrency_key'
-    t.text 'cron_key'
-    t.uuid 'retried_good_job_id'
-    t.datetime 'cron_at'
-    t.index %w[active_job_id created_at], name: 'index_good_jobs_on_active_job_id_and_created_at'
-    t.index ['active_job_id'], name: 'index_good_jobs_on_active_job_id'
-    t.index ['concurrency_key'], name: 'index_good_jobs_on_concurrency_key_when_unfinished',
-                                 where: '(finished_at IS NULL)'
-    t.index %w[cron_key created_at], name: 'index_good_jobs_on_cron_key_and_created_at'
-    t.index %w[cron_key cron_at], name: 'index_good_jobs_on_cron_key_and_cron_at', unique: true
-    t.index ['finished_at'], name: 'index_good_jobs_jobs_on_finished_at',
-                             where: '((retried_good_job_id IS NULL) AND (finished_at IS NOT NULL))'
-    t.index %w[queue_name scheduled_at], name: 'index_good_jobs_on_queue_name_and_scheduled_at',
-                                         where: '(finished_at IS NULL)'
-    t.index ['scheduled_at'], name: 'index_good_jobs_on_scheduled_at', where: '(finished_at IS NULL)'
+  create_table "good_jobs", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.text "queue_name"
+    t.integer "priority"
+    t.jsonb "serialized_params"
+    t.datetime "scheduled_at", precision: nil
+    t.datetime "performed_at", precision: nil
+    t.datetime "finished_at", precision: nil
+    t.text "error"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.uuid "active_job_id"
+    t.text "concurrency_key"
+    t.text "cron_key"
+    t.uuid "retried_good_job_id"
+    t.datetime "cron_at", precision: nil
+    t.index ["active_job_id", "created_at"], name: "index_good_jobs_on_active_job_id_and_created_at"
+    t.index ["active_job_id"], name: "index_good_jobs_on_active_job_id"
+    t.index ["concurrency_key"], name: "index_good_jobs_on_concurrency_key_when_unfinished", where: "(finished_at IS NULL)"
+    t.index ["cron_key", "created_at"], name: "index_good_jobs_on_cron_key_and_created_at"
+    t.index ["cron_key", "cron_at"], name: "index_good_jobs_on_cron_key_and_cron_at", unique: true
+    t.index ["finished_at"], name: "index_good_jobs_jobs_on_finished_at", where: "((retried_good_job_id IS NULL) AND (finished_at IS NOT NULL))"
+    t.index ["queue_name", "scheduled_at"], name: "index_good_jobs_on_queue_name_and_scheduled_at", where: "(finished_at IS NULL)"
+    t.index ["scheduled_at"], name: "index_good_jobs_on_scheduled_at", where: "(finished_at IS NULL)"
   end
 
-  create_table 'orders', force: :cascade do |t|
-    t.bigint 'user_id', null: false
-    t.decimal 'total', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['user_id'], name: 'index_orders_on_user_id'
-    t.check_constraint 'total >= 0::numeric', name: 'totalchk'
+  create_table "orders", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.decimal "total", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
+    t.check_constraint "total >= 0::numeric", name: "totalchk"
   end
 
-  create_table 'placements', force: :cascade do |t|
-    t.bigint 'order_id', null: false
-    t.bigint 'product_id', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.integer 'quantity', default: 0, null: false
-    t.index ['order_id'], name: 'index_placements_on_order_id'
-    t.index ['product_id'], name: 'index_placements_on_product_id'
+  create_table "placements", force: :cascade do |t|
+    t.bigint "order_id", null: false
+    t.bigint "product_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "quantity", default: 0, null: false
+    t.index ["order_id"], name: "index_placements_on_order_id"
+    t.index ["product_id"], name: "index_placements_on_product_id"
   end
 
-  create_table 'products', force: :cascade do |t|
-    t.string 'title', null: false
-    t.decimal 'price', default: '0.0', null: false
-    t.boolean 'published', default: false, null: false
-    t.bigint 'user_id', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.integer 'quantity', default: 0, null: false
-    t.index ['user_id'], name: 'index_products_on_user_id'
-    t.check_constraint 'price >= 0::numeric', name: 'pricechk'
+  create_table "products", force: :cascade do |t|
+    t.string "title", null: false
+    t.decimal "price", default: "0.0", null: false
+    t.boolean "published", default: false, null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "quantity", default: 0, null: false
+    t.index ["user_id"], name: "index_products_on_user_id"
+    t.check_constraint "price >= 0::numeric", name: "pricechk"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'email', null: false
-    t.string 'password_digest', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index 'lower((email)::text)', name: 'index_users_on_lowercase_email', unique: true
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index "lower((email)::text)", name: "index_users_on_lowercase_email", unique: true
   end
 
-  add_foreign_key 'orders', 'users'
-  add_foreign_key 'placements', 'orders'
-  add_foreign_key 'placements', 'products'
-  add_foreign_key 'products', 'users'
+  add_foreign_key "orders", "users"
+  add_foreign_key "placements", "orders"
+  add_foreign_key "placements", "products"
+  add_foreign_key "products", "users"
 end
